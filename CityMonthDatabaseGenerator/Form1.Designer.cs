bcb801d4e85f5401d775778ba0861f5a89648fe6
playmakerbgt <bjt32865@gmail.com>
2016-12-22 17:33:04 -0500

    12/22 Laptop
    Version 1.0

---
+++ CityMonthDatabaseGenerator/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace CityMonthDatabaseGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.runButton = new System.Windows.Forms.Button();
+            this.filePathTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.yearComboBox = new System.Windows.Forms.ComboBox();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(339, 118);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 23);
+            this.runButton.TabIndex = 0;
+            this.runButton.Text = "Run";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // filePathTextBox
+            // 
+            this.filePathTextBox.Location = new System.Drawing.Point(82, 58);
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.Size = new System.Drawing.Size(441, 20);
+            this.filePathTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Path:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(540, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Year:";
+            // 
+            // yearComboBox
+            // 
+            this.yearComboBox.FormattingEnabled = true;
+            this.yearComboBox.Items.AddRange(new object[] {
+            "2014",
+            "2015",
+            "2016",
+            "2017",
+            "2018",
+            "2019"});
+            this.yearComboBox.Location = new System.Drawing.Point(578, 58);
+            this.yearComboBox.Name = "yearComboBox";
+            this.yearComboBox.Size = new System.Drawing.Size(122, 21);
+            this.yearComboBox.TabIndex = 4;
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.Location = new System.Drawing.Point(339, 157);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(0, 13);
+            this.errorLabel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 200);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.yearComboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filePathTextBox);
+            this.Controls.Add(this.runButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.TextBox filePathTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox yearComboBox;
+        private System.Windows.Forms.Label errorLabel;
+    }
+}
+
---
+++ CityMonthDatabaseGenerator/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+- remove all comma's, ctrl-f then delete
+- Steps: Save file as a .csv.
+- copy output statements into sql
+*/
+
+namespace CityMonthDatabaseGenerator
+{
+    public partial class Form1 : Form
+    {
+        PerDiemAmountsEntities db = new PerDiemAmountsEntities();
+        List<string> months = new List<string>() { "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC" };
+        List<string> files;
+        List<string> listA;
+        string state, city, basepath, seasonBegin, seasonEnd;
+        int lodging, meals, row, total, year;
+
+        public Form1()
+        {
+            InitializeComponent();
+            yearComboBox.SelectedIndex = 1;
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            Initialize();
+            DirectoryInfo d = new DirectoryInfo(basepath);
+            FileInfo[] Files = d.GetFiles("*.csv"); //Getting csv files
+            foreach (FileInfo file in Files)
+            {
+                files.Add(basepath + "\\" + file.Name);
+            }
+            if(files.Count > 0)
+            {
+                foreach (string file in files)
+                {
+                    try
+                    {
+                        StreamReader reader = new StreamReader(File.OpenRead(file), Encoding.UTF8);
+                        listA = new List<string>();
+                        while (!reader.EndOfStream)
+                        {
+                            #region starting lines
+                            if (row == 1)
+                            {
+                                reader.ReadLine();
+                                reader.ReadLine();
+                                row++;
+                            }
+                            #endregion
+                            ReadLine(reader);
+                        }
+                        reader.Close();
+                        errorLabel.Text = "Complete";
+                    }
+                    catch (Exception ex)
+                    { Console.Write(ex); }
+                }
+                
+            }
+        }
+
+        private void ReadLine(StreamReader reader)
+        {
+            bool start = false, monthsSwitched = false; ;
+            var line = reader.ReadLine();
+            var values = line.Split(';');
+            listA = ((string)values[0]).Split(',').ToList();
+            if (listA.ElementAt(0) != "")
+            {
+                try
+                {
+                    state = listA.ElementAt(1);
+                    if (city != listA.ElementAt(2))
+                        city = listA.ElementAt(2);
+                    seasonBegin = listA.ElementAt(4);
+                    seasonEnd = listA.ElementAt(5);
+                    lodging = Int32.Parse(listA.ElementAt(listA.Count - 2).Substring(2));
+                    meals = Int32.Parse(listA.ElementAt(listA.Count - 1).Substring(2));
+                    total = lodging + meals;
+                    var cityPerDiem = (from p in db.CityPerDiems where p.City == city && p.State == state select p).FirstOrDefault();
+                    if (cityPerDiem != null)
+                    {
+                        if (seasonEnd == "")
+                        {
+                            foreach (string s in months)
+                            {
+                                Console.WriteLine("insert into " + s + "PerDiem(Year, Meals, Lodging, Total, CityId) values(" + year +
+                                    ", " + meals + ", " + lodging + ", " + total + ", " + cityPerDiem.Id + ")");
+                            }
+                        }
+                        else
+                        {
+                            foreach (string s in months)
+                            {
+                                if (s == seasonBegin.Substring(0, 3).ToUpper())
+                                {
+                                    start = true;
+                                }
+                                if (start)
+                                {
+                                    Console.WriteLine("insert into " + s + "PerDiem(Year, Meals, Lodging, Total, CityId) values(" + year +
+                                    ", " + meals + ", " + lodging + ", " + total + ", " + cityPerDiem.Id + ")");
+                                }
+                                if (s == seasonEnd.Substring(0, 3).ToUpper() && start)
+                                {
+                                    break;
+                                }
+                                else if (s == "DEC")
+                                {
+                                    monthsSwitched = true;
+                                }
+                            }
+                            if (monthsSwitched)
+                            {
+                                foreach (string s in months)
+                                {
+                                    Console.WriteLine("insert into " + s + "PerDiem(Year, Meals, Lodging, Total, CityId) values(" + year +
+                                    ", " + meals + ", " + lodging + ", " + total + ", " + cityPerDiem.Id + ")");
+                                    if (s == seasonEnd.Substring(0, 3).ToUpper())
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex) { }
+            }
+        }
+
+        private void Initialize()
+        {
+            basepath = filePathTextBox.Text;
+            state = "";
+            city = "";
+            lodging = 0;
+            meals = 0;
+            files = new List<string>();
+            row = 1;
+            year = Int32.Parse(yearComboBox.SelectedItem.ToString());
+        }
+    }
+}
